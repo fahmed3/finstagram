78cd282be2f22e154bbd97f147615fe46e790a27
Fabiha Ahmed <fsafabiha@gmail.com>
2019-12-10 20:36:42 -0500

    working

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask, render_template, request, session, redirect, url_for, send_file
+from flask import Flask, render_template, request, session, redirect, url_for, send_file, flash
 import os
 import uuid
 import hashlib
 import pymysql.cursors
 from functools import wraps
 import time
-
+1
 app = Flask(__name__)
 app.secret_key = "super secret key"
 IMAGES_DIR = os.path.join(os.getcwd(), "images")
@@ -36,7 +36,148 @@ def index():
 @app.route("/home")
 @login_required
 def home():
-    return render_template("home.html", username=session["username"])
+    #get all unresponded to requests
+    query = "SELECT f.username_follower, firstName, lastName FROM follow AS f JOIN person AS p ON p.username = f.username_follower WHERE username_followed = %s AND followstatus=0"
+    with connection.cursor() as cursor:
+        cursor.execute(query, (session["username"]))
+    requests = cursor.fetchall()
+    #get all followers
+    query = "SELECT firstName, lastName FROM follow AS f JOIN person AS p ON p.username = f.username_follower WHERE username_followed = %s AND followstatus=1"
+    with connection.cursor() as cursor:
+        cursor.execute(query, (session["username"]))
+    followers = cursor.fetchall()
+    return render_template("home.html", username=session["username"], requests=requests, followers=followers)
+
+@app.route("/groups")
+@login_required
+def groups():
+    query = "SELECT * FROM friendgroup WHERE groupOwner = %s"
+    with connection.cursor() as cursor:
+        cursor.execute(query, session['username'])
+    data = cursor.fetchall()
+    return render_template("groups.html", groups = data)
+
+@app.route("/newgroup")
+@login_required
+def newgroup():
+    return render_template("newgroup.html")
+
+@app.route("/createnewgroup", methods=["GET", "POST"])
+@login_required
+def createnewgroup():
+    if request.form:
+        name = request.form["groupName"]
+        description = request.form["description"]    
+    with connection.cursor() as cursor:
+        query = "SELECT * FROM friendgroup WHERE groupOwner = %s AND groupName = %s"
+        cursor.execute(query, (session['username'], name))
+        data = cursor.fetchone()
+        if data:
+            message = "Failed to create group, you already have a group with that name."
+            flash(message)
+            return redirect(url_for("groups"))
+        else:
+            #create new group
+            query = "INSERT INTO friendgroup VALUES (%s, %s, %s);"
+            cursor.execute(query, (session['username'], name, description ))
+            #add user as member of the group they just created
+            query = "INSERT INTO belongto VALUES (%s, %s, %s);"
+            cursor.execute(query,(session['username'],session['username'],name))
+            message = "Successfully created new friend group."
+            flash(message)
+            return redirect(url_for("groups"))            
+
+@app.route("/groupdetails", methods=["GET", "POST"])
+@login_required
+def groupdetails():
+    if request.form:
+        groupdetails = request.form["details"]
+        g = groupdetails.split(",")
+        groupOwner = g[0]
+        groupName = g[1]
+    query = "SELECT member_username FROM belongto WHERE owner_username = %s AND groupName = %s"
+    with connection.cursor() as cursor:
+        cursor.execute(query, (groupOwner, groupName))
+    data = cursor.fetchall()
+    return render_template("groupdetails.html",
+                           members = data,
+                           groupName = groupName)
+
+@app.route("/addmember", methods=["GET", "POST"])
+@login_required
+def addmember():
+    if request.form:
+        username = request.form["username"]
+        groupName = request.form["groupdetails"]
+    query = "SELECT * FROM person WHERE username = %s"
+    with connection.cursor() as cursor:
+        cursor.execute(query, (username))
+    data = cursor.fetchone()
+    if data: #if username exists
+        query = "SELECT * FROM belongto WHERE member_username = %s AND owner_username = %s AND groupName = %s"
+        with connection.cursor() as cursor:
+            cursor.execute(query, (username, session['username'], groupName))
+        data = cursor.fetchone()
+        if data: #if username in group already
+            message = username + " is already in the group!"
+            return render_template("addmember.html", success = False, message=message)
+        else:
+            query = "INSERT INTO belongto VALUES (%s, %s, %s);"
+            with connection.cursor() as cursor:
+                cursor.execute(query, (username, session['username'], groupName))
+            return render_template("addmember.html", success = True)
+    else:
+        message = "Please make sure to type in the username correctly." 
+        return render_template("addmember.html", success = False, message = message)
+
+
+    #MAKE THIS WORK
+@app.route("/follow", methods=["GET", "POST"])
+@login_required
+def follow():
+    if request.form:
+        username = request.form["username"]
+    query = "SELECT username, firstName, lastName, bio FROM person WHERE username = %s"
+    with connection.cursor() as cursor:
+        cursor.execute(query, (username))
+    data = cursor.fetchone()
+    if data:
+        return render_template("follow.html", userfound = True, user = data)
+    else:
+        message = "No user found with this username, please make sure you type in the username correctly."
+        return render_template("follow.html", userfound = False)
+
+@app.route("/sendfollow", methods=["GET", "POST"])
+@login_required
+def sendfollow():
+    if request.form:
+        username = request.form["follow"]
+    query = "INSERT INTO follow VALUES (%s, %s, %s)"
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(query, (username, session["username"], 0))
+        flash("Follow requested.")
+    except pymysql.err.IntegrityError:
+        flash("You've already requested to follow this user.")
+    return redirect(url_for("home"))
+
+@app.route("/accept", methods=["GET", "POST"])
+@login_required
+def accept():
+    if request.form:
+        if "accept" in request.form:
+            username = request.form["accept"]
+            query = "UPDATE follow SET followstatus = 1 WHERE username_follower = %s AND username_followed = %s"
+            with connection.cursor() as cursor:
+                cursor.execute(query, (username, session["username"]))
+            flash(username + " is now a follower.")
+        if "decline" in request.form:
+            username = request.form["decline"]
+            query = "DELETE FROM follow WHERE username_follower = %s AND username_followed = %s"
+            with connection.cursor() as cursor:
+                cursor.execute(query, (username, session["username"]))
+            flash("You've declined the follow request by " + username + ".")
+    return redirect(url_for("home"))
 
 @app.route("/upload", methods=["GET"])
 @login_required
@@ -50,11 +191,44 @@ def upload():
 @app.route("/images", methods=["GET"])
 @login_required
 def images():
-    query = "SELECT * FROM photo WHERE AllFollowers AND (photoPoster) IN (SELECT username_followed FROM follow WHERE followStatus AND username_follower = %s) UNION ( SELECT photoID, postingdate, filepath, allFollowers, caption, photoPoster FROM photo NATURAL JOIN SharedWith WHERE (groupOwner, groupName) IN (SELECT owner_username, groupName FROM BelongTo WHERE member_username = %s) ) ORDER BY postingDate DESC"
+    #visible_photos view created once
+    #query = "CREATE VIEW AS SELECT * FROM photo WHERE AllFollowers AND (photoPoster) IN (SELECT username_followed FROM follow WHERE followStatus AND username_follower = %s) UNION ( SELECT photoID, postingdate, filepath, allFollowers, caption, photoPoster FROM photo NATURAL JOIN SharedWith WHERE (groupOwner, groupName) IN (SELECT owner_username, groupName FROM BelongTo WHERE member_username = %s) ) ORDER BY postingDate DESC"
+    #with connection.cursor() as cursor:
+    #    cursor.execute(query, (session['username'], session['username']) )
+
+    #also only executed once
+    #view to get the number of likes and tagged people on each post
+    #"CREATE VIEW numLikesAndTags AS SELECT photoID, IFNULL(n, 0) AS numtagged, numlikes FROM visible_photos LEFT JOIN (SELECT photoID, count(DISTINCT username) AS n FROM visible_photos LEFT JOIN tagged USING (photoID) WHERE tagstatus = 1 GROUP BY photoID) AS numTags USING (photoID) LEFT JOIN (SELECT photoID, count(liketime) AS numlikes FROM visible_photos LEFT JOIN likes USING (photoID) GROUP BY photoID) AS numoflikes USING (photoID)"
+    
+    query = "SELECT photoID, postingdate, filepath, allFollowers, caption, photoPoster, firstName, lastName, numLikes, numTagged FROM visible_photos JOIN person ON photoPoster = username JOIN numlikesandtags USING (photoID) ORDER BY postingdate DESC"
     with connection.cursor() as cursor:
-        cursor.execute(query, (session['username'], session['username']) )
-    data = cursor.fetchall()
-    return render_template("images.html", images=data)
+        cursor.execute(query)
+    images = cursor.fetchall()
+
+    #if likes is selected for a photo, details is true
+    details = request.args.get('details')    
+    if details:
+        photoID = request.args.get('photoID')
+        #get all likes and ratings for photo
+        query = "SELECT username, rating FROM likes WHERE photoID = %s ORDER BY liketime DESC"
+        with connection.cursor() as cursor:
+            cursor.execute(query, (photoID))
+        likes = cursor.fetchall()
+        query = "SELECT username FROM tagged WHERE photoID = %s"
+        with connection.cursor() as cursor:
+            cursor.execute(query, (photoID))
+        tags = cursor.fetchall()
+        return render_template("images.html",
+                               details= details,
+                               likes = likes,
+                               tags = tags)
+    return render_template("images.html", images=images)
+
+@app.route("/image", methods=["GET", "POST"])
+def imageDetails():
+    if request.method == 'POST':
+        photoID = request.form['photoID']
+    return redirect(url_for("images", details=True, photoID = photoID))
 
 @app.route("/image/<image_name>", methods=["GET"])
 def image(image_name):
@@ -135,12 +309,11 @@ def upload_image():
         
         if "allFollowers" in request.form:
             allFollowers = True
-            with connection.cursor() as cursor:
-                cursor.execute(query, (time.strftime('%Y-%m-%d %H:%M:%S'), image_name, allFollowers, caption, session["username"]))
-        else:
-            with connection.cursor() as cursor:
-                cursor.execute(query, (time.strftime('%Y-%m-%d %H:%M:%S'), image_name, allFollowers, caption, session["username"]))
-                id = cursor.lastrowid #gets id of last inserted row
+
+        with connection.cursor() as cursor:
+            cursor.execute(query, (time.strftime('%Y-%m-%d %H:%M:%S'), image_name, allFollowers, caption, session["username"]))
+        if "friendgroup" in request.form:
+            id = cursor.lastrowid #gets id of last inserted row
             #print(id)
             groups = request.form.getlist("friendgroup") #list of friend groups from checkboxes
             shareWithQuery = "INSERT INTO SharedWith (groupOwner, groupName, photoID) VALUES (%s, %s, %s)"
@@ -152,10 +325,14 @@ def upload_image():
                 with connection.cursor() as cursor:
                     cursor.execute(shareWithQuery, (g[0], g[1], id) )
         message = "Image has been successfully uploaded."
-        return render_template("upload.html", message=message)
+        flash(message) #added flash since only difference between upload and uploadImage was the message
+        return redirect(url_for("upload"))
+        #return render_template("upload.html", message=message)
     else:
         message = "Failed to upload image."
-        return render_template("upload.html", message=message)
+        flash(message)
+        return redirect(url_for("upload"))
+        #return render_template("upload.html", message=message)
 
 if __name__ == "__main__":
     if not os.path.isdir("images"):
